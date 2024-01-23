8de6266a9a652477c82056c75d1df8a020262eca
salbyul <kpast96@gmail.com>
2024-01-23 21:51:20 +0900

    feat: 타이틀이 비어있을 경우 메시지 추가

--- src/main/java/org/winternote/winternote/controller/utils/message/Message.java
+++ src/main/java/org/winternote/winternote/controller/utils/message/Message.java
@@ -2,7 +2,9 @@ package org.winternote.winternote.controller.utils.message;
 
 public enum Message {
 
-    CONFIGURATION_ERROR("설정 파일에 문제가 있습니다.");
+    CONFIGURATION_ERROR("설정 파일에 문제가 있습니다."),
+
+    TITLE_EMPTY_ERROR("Title cannot be empty");
 
     private final String content;
 
