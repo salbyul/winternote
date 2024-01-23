858eca9b329e0903b72a1dde369f83ed344151cb
salbyul <kpast96@gmail.com>
2024-01-23 16:48:40 +0900

    feat: Starter 창 사이즈 조절

--- src/main/java/org/winternote/winternote/WinterNoteApplication.java
+++ src/main/java/org/winternote/winternote/WinterNoteApplication.java
@@ -7,14 +7,17 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+import static org.winternote.winternote.property.PrivateProperty.*;
+
 public class WinterNoteApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("winter-note-starter.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        Scene scene = new Scene(fxmlLoader.load(), DISPLAY_WIDTH / 3, DISPLAY_HEIGHT / 2);
         stage.setTitle("Winter Note");
         stage.setScene(scene);
+
         stage.show();
     }
 
--- src/main/java/org/winternote/winternote/controller/StarterController.java
+++ src/main/java/org/winternote/winternote/controller/StarterController.java
@@ -2,7 +2,11 @@ package org.winternote.winternote.controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -11,12 +15,25 @@ import java.io.IOException;
 public class StarterController {
 
     @FXML
+    private VBox screen;
+
+    @FXML
+    private Button newButton;
+
+    @FXML
+    private ScrollPane scrollPane;
+
+    public void initialize() {
+        screen.setAlignment(Pos.CENTER);
+    }
+
+    @FXML
     private void onNewButtonClick() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("creation-note.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        Stage stage = new Stage();
-        stage.setScene(scene);
-        stage.initModality(Modality.APPLICATION_MODAL);
-        stage.show();
+        Stage newStage = new Stage();
+        newStage.setScene(scene);
+        newStage.initModality(Modality.APPLICATION_MODAL);
+        newStage.show();
     }
 }
