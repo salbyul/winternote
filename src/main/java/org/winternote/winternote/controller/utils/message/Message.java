113c9f6f859230986062f04bb2c8fc3f34a04b16
salbyul <kpast96@gmail.com>
2024-01-23 23:27:55 +0900

    feat: 알려지지 않은 에러 메시지 추가

--- src/main/java/org/winternote/winternote/controller/utils/message/Message.java
+++ src/main/java/org/winternote/winternote/controller/utils/message/Message.java
@@ -3,6 +3,7 @@ package org.winternote.winternote.controller.utils.message;
 public enum Message {
 
     CONFIGURATION_ERROR("There is a problem with settings file."),
+    UNKNOWN_ERROR("Unknown Error occurred."),
 
     TITLE_EMPTY_ERROR("Title cannot be empty.");
 
