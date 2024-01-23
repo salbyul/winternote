fc35ebf8212943120444999d6d2b31ace04b7611
salbyul <kpast96@gmail.com>
2024-01-23 23:54:31 +0900

    fix: 버그 제거

--- src/main/java/org/winternote/winternote/controller/CreationController.java
+++ src/main/java/org/winternote/winternote/controller/CreationController.java
@@ -68,7 +68,7 @@ public class CreationController extends AbstractController {
 
     protected static Stage generateStage() {
         return AbstractController.generateStage(() -> {
-            FXMLLoader fxmlLoader = new FXMLLoader(CreationController.class.getResource("creat1ion-note.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(CreationController.class.getResource("creation-note.fxml"));
             Scene scene = new Scene(fxmlLoader.load(), DISPLAY_WIDTH / 5, DISPLAY_HEIGHT / 4);
             Stage newStage = new Stage();
             newStage.setScene(scene);
