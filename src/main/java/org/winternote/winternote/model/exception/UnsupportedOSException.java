f5de5d20bc5f4dc632ca3d323acd0e0686e938c7
salbyul <kpast96@gmail.com>
2024-01-26 02:21:03 +0900

    refactor: 예외 이름 변경

---
+++ src/main/java/org/winternote/winternote/model/exception/UnsupportedOSException.java
@@ -0,0 +1,8 @@
+package org.winternote.winternote.model.exception;
+
+public class UnsupportedOSException extends RuntimeException {
+
+    public UnsupportedOSException(final String message) {
+        super(message);
+    }
+}
