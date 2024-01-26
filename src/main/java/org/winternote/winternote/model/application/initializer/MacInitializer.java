c36aff86ceea484c74fd9fe1985388e1756a098d
salbyul <kpast96@gmail.com>
2024-01-26 16:13:38 +0900

    refactor: 초기 프로젝트 경로 변경

--- src/main/java/org/winternote/winternote/model/application/initializer/MacInitializer.java
+++ src/main/java/org/winternote/winternote/model/application/initializer/MacInitializer.java
@@ -3,9 +3,6 @@ package org.winternote.winternote.model.application.initializer;
 import org.winternote.winternote.model.exception.UnknownException;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import static org.winternote.winternote.model.property.PrivateProperty.*;
 import static org.winternote.winternote.model.property.PrivateProperty.USER_NAME;
@@ -24,7 +21,7 @@ public class MacInitializer implements Initializer {
     private void initializeMetaDataFile() {
         File file = new File(APPLICATION_PATH + "/metadata.wn");
         try (PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(file)))) {
-            writer.print("Location: " + APPLICATION_PATH + "/projects\n");
+            writer.print("Location: " + APPLICATION_PATH + "/Desktop\n");
             writer.print("recent projects: [\n]");
             writer.flush();
         } catch (IOException e) {
@@ -33,7 +30,7 @@ public class MacInitializer implements Initializer {
     }
 
     private static void initializeDefaultDirectoryStructure() {
-        boolean isCreated = new File("/Users/" + USER_NAME + "/winternote/projects").mkdirs();
+        boolean isCreated = new File("/Users/" + USER_NAME + "/winternote").mkdirs();
         if (!isCreated) {
             throw new UnknownException("Could not initialize");
         }
@@ -41,8 +38,7 @@ public class MacInitializer implements Initializer {
 
     @Override
     public boolean isFirstTimeRunning() {
-        Path path = Paths.get(APPLICATION_PATH + "/projects");
         File metadata = new File(APPLICATION_PATH + "/metadata.wn");
-        return !Files.exists(path) || !metadata.exists();
+        return !metadata.exists();
     }
 }
