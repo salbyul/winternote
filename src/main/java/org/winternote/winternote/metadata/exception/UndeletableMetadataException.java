e5a3e0c5ff49f68c5810c1fa1fab463ccafb0aec
salbyul <kpast96@gmail.com>
2024-02-13 23:45:00 +0900

    refactor: fix typo.

--- src/main/java/org/winternote/winternote/metadata/exception/UndeletableMetadataException.java
+++ src/main/java/org/winternote/winternote/metadata/exception/UndeletableMetadataException.java
@@ -2,9 +2,9 @@ package org.winternote.winternote.metadata.exception;
 
 import org.winternote.winternote.common.exception.WinterException;
 
-public class UndeleteableMetadataException extends WinterException {
+public class UndeletableMetadataException extends WinterException {
 
-    public UndeleteableMetadataException(final String message) {
+    public UndeletableMetadataException(final String message) {
         super(message);
     }
 }
